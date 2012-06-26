6c9652547ede0d039620b5e89087347ca8dc3802
scrambled <scrambled@xirinet.com>
2012-06-27 00:45:43 +0700

    fewp

--- bin/chav.pl
+++ bin/chav.pl
@@ -8,24 +8,19 @@ $| = 1;
 
 my $mongo  = MongoDB::Connection->new();
 my $db     = $mongo->get_database('wot-replays');
-my $c      = $db->get_collection('replays.chat');
+my $c      = $db->get_collection('replays');
+my $cc     = $db->get_collection('replays.chat');
+my $data   = {};
 
-my $cursor = $c->find({ channel => 'unknown' })->sort({ replay_id => 1, sequence => 1 }); 
-
-my $data = {};
-
-while(my $o = $cursor->next()) {
-    push(@{$data->{$o->{replay_id}}}, {
-        s => $o->{source},
-        b => $o->{body}
-    });
+my $cursor = $c->find({ chatProcessed => true })->sort({ 'site.uploaded_at' => 1 });
+while(my $r = $cursor->next()) {
+    $data->{$r->{_id}} = [ $cc->find({ replay_id => $r->{_id} })->sort({ sequence => 1 })->all() ];
 }
-
 foreach my $id (keys(%$data)) {
     print $id, "\n";
 
     foreach my $m (@{$data->{$id}}) {
-        print "\t", sprintf('% 20s | %s', $m->{s}, $m->{b}), "\n";
+        print "\t", sprintf('% 20s | %s', $m->{source}, $m->{body}), "\n";
     }
     print "\n\n";
 }
