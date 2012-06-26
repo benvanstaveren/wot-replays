1cae88a6d30fbec5af5d15afbb3ec8d62636de47
scrambled <scrambled@xirinet.com>
2012-06-26 23:49:20 +0700

    removedchav

--- bin/chav.pl
+++ bin/chav.pl
@@ -10,4 +10,22 @@ my $mongo  = MongoDB::Connection->new();
 my $db     = $mongo->get_database('wot-replays');
 my $c      = $db->get_collection('replays.chat');
 
-my $cursor = $c->find({ channel => 'unknown' })->
+my $cursor = $c->find({ channel => 'unknown' }); 
+
+my $data = {};
+
+while(my $o = $cursor->next()) {
+    $data->{$o->{replay_id}}->[ $o->{sequence} ] = { 
+        s => $o->{source},
+        b => $o->{body}
+    };
+}
+
+foreach my $id (keys(%$data)) {
+    print $id, "\n";
+
+    foreach my $m (@{$data->{$id}}) {
+        print "\t", sprintf('<% 20s>', $m->{s}), ' ', $m->{b}, "\n";
+    }
+    print "\n\n";
+}
