79393955a3e73e9aa84cb4293443ba5bcaacffe9
scrambled <scrambled@xirinet.com>
2012-07-18 02:54:16 +0700

    fix

--- etc/mr/stats_bytier/map.js
+++ etc/mr/stats_bytier/map.js
@@ -1,5 +1,5 @@
 function() {
     // find the vehicle and get it's class
     var vehicle = db['data.vehicles'].findOne({ _id: this.player.vehicle.full });
-    if(vehicle) emit(vehicle.level, 1);
+    if(vehicle && vehicle.type) emit(vehicle.level, 1);
 }
