a0af648341406f1a86326831834c8b0300d997e5
scrambled <scrambled@xirinet.com>
2012-12-31 04:41:48 +0700

    moar misc

--- lib/WR/App/Controller/Replays/Upload.pm
+++ lib/WR/App/Controller/Replays/Upload.pm
@@ -70,7 +70,6 @@ sub upload {
             };
 
             return $self->r_error(sprintf('Error parsing replay: %s', $pe), $replay_file) if($pe);
-            return $self->r_error('There is an issue with clan wars replays at the moment that prevents them from being parsed properly, sorry! Can\'t store it...', $replay_file) unless(defined($m_data->{_id}) && !ref($m_data->{_id}));
             return $self->r_error('That replay seems to exist already', $replay_file) if($self->db('wot-replays')->get_collection('replays')->find_one({ _id => $m_data->{_id} }));
             return $self->r_error('That replay seems to be coming from the public test server, we can\'t store those at the moment', $replay_file) if($m_data->{player}->{name} =~ /.*_(EU|NA|RU|SEA|US)$/);
             return $self->r_error(q|Courtesy of WG, this replay can't be stored, it's missing your player ID, and we use that to uniquely identify each player|, $replay_file) if($m_data->{player}->{id} == 0);
