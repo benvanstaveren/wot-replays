54c482c346e3f78e2857321e1ae68838eb74e24b
scrambled <scrambled@xirinet.com>
2013-01-02 21:59:04 +0700

    fixed download link

--- lib/WR/App/Controller/Replays/Export.pm
+++ lib/WR/App/Controller/Replays/Export.pm
@@ -7,10 +7,8 @@ sub download {
     my $self = shift;
     my $id   = $self->stash('replay_id');
 
-    if(my $replay = $self->db('wot-replays')->get_collection('replays')->find_one({ _id => $id })) {
-        $self->db('wot-replays')->get_collection('replays')->update({ _id => $id }, { '$inc' => { 'site.downloads' => 1 } });
-
-        # the link needs to be URL encoded so we might as well make it a proper URL
+    if(my $replay = $self->db('wot-replays')->get_collection('replays')->find_one({ _id => bless({ value => $id }, 'MongoDB::OID') })) {
+        $self->db('wot-replays')->get_collection('replays')->update({ _id => $replay->{_id} }, { '$inc' => { 'site.downloads' => 1 } });
         my $url = Mojo::URL->new(sprintf('http://dl.wot-replays.org/%s', $replay->{file}));
         $self->redirect_to($url->to_string);
     } else {
