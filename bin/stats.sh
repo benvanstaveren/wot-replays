e078c549a9d576e7f243330be97c9641135588ce
scrambled <scrambled@xirinet.com>
2013-01-05 12:36:55 +0700

    added stats script

---
+++ bin/stats.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/wotreplay/wot-replays/bin
+./mr.pl ../etc/mr/stats_bybonustype stats.bybonustype
+./mr.pl ../etc/mr/stats_byclass stats.byclass
+./mr.pl ../etc/mr/stats_bycountry stats.bycountry
+./mr.pl ../etc/mr/stats_bygametype stats.bygametype
+./mr.pl ../etc/mr/stats_bytier stats.bytier
+./mr.pl ../etc/mr/stats_byversion stats.byversion
