f491b15b451be1c90a93d57f831124a0e8e1de06
scrambled <scrambled@xirinet.com>
2013-01-13 20:03:35 +0700

    foo

--- bin/res2.pl
+++ bin/res2.pl
@@ -5,3 +5,5 @@ use WR::Res::Achievements;
 
 my $res = WR::Res::Achievements->new();
 print $res->is_award($ARGV[0]), "\n";
+print $res->is_battle($ARGV[0]), "\n";
+print $res->index_to_idstr($ARGV[0]), "\n";
