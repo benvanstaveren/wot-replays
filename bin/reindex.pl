27168995ca1f0319ff494a024bec5539e7fff0fc
scrambled <scrambled@xirinet.com>
2013-01-14 18:28:09 +0700

    foo

--- bin/reindex.pl
+++ bin/reindex.pl
@@ -8,7 +8,7 @@ use WR::Elastic;
 
 $| = 1;
 
-my $mongo  = MongoDB::Connection->new();
+my $mongo  = MongoDB::Connection->new(host => 'mongodb://hwn-01.blockstackers.net:27017');
 my $db     = $mongo->get_database('wot-replays');
 my $rc     = $db->get_collection('replays')->find()->sort({ 'site.uploaded_at' => -1 });
 
@@ -17,5 +17,5 @@ my $elastic = WR::Elastic->new();
 $elastic->setup if($ARGV[0] eq 'setup');
 
 while(my $r = $rc->next()) {
-    $elastic->index_replay($r);
+    $elastic->index($r);
 }
