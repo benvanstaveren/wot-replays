4d85c339988697b6f26761065d415697425cefba
scrambled <scrambled@xirinet.com>
2013-01-16 19:44:04 +0700

    foo

--- bin/parse.pl
+++ bin/parse.pl
@@ -15,7 +15,7 @@ $| = 1;
 use constant WOT_BF_KEY_STR => 'DE 72 BE A0 DE 04 BE B1 DE FE BE EF DE AD BE EF';
 use constant WOT_BF_KEY     => join('', map { chr(hex($_)) } (split(/\s/, WOT_BF_KEY_STR)));
 
-my $mongo  = MongoDB::Connection->new();
+my $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
 my $db     = $mongo->get_database('wot-replays');
 
 my $process = WR::Process->new(file => $ARGV[0], db => $db, bf_key => WOT_BF_KEY);
