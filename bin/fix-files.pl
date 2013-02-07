0da855779f4804c14ed8c2ff5f7df0c29b2e95ec
scrambled <scrambled@xirinet.com>
2013-02-07 11:13:07 +0700

    fixfiles

---
+++ bin/fix-files.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use MongoDB;
+use Try::Tiny;
+use Data::Dumper;
+use File::Path qw/make_path/;
+use File::Copy qw/move/;
+
+$| = 1;
+
+my $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
+my $db     = $mongo->get_database('wot-replays');
+
+my $query = {
+    'site.uploaded_at' => {
+        '$lte' => time()
+    },
+};
+
+my $rc = $db->get_collection('replays')->find($query)->sort({ 'site.uploaded_at' => -1 });
+
+
+my $path = (-e '/home/ben') 
+    ? '/home/ben/projects/wot-replays/data/replays'
+    : '/home/wotreplay/wot-replays/data/replays';
+
+use DateTime;
+
+while(my $r = $rc->next()) {
+    print 'no file', "\n" and next unless(defined($r->{file}));
+    print 'already', "\n" and next if($r->{file} =~ /(d{4})\/(\d{2})\/(\d{2})/);
+
+    my $dt = DateTime->from_epoch(epoch => $r->{_id}->get_time);
+
+    my $_path = sprintf('%s/%s', $path, $dt->strftime('%Y/%m/%d'));
+    make_path($_path);
+
+    my $new_file = sprintf('%s/%s', $dt->strftime('%Y/%m/%d'), $r->{file});
+    my $dst_file = sprintf('%s/%s', $_path, $r->{file});
+
+    print 'move(', sprintf('%s/%s', $path, $r->{file}), ' => ', $dst_file, ');', "\n";
+}
