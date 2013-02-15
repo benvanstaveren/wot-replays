35f0126c4f8203e536dd5de99a5da9e45348d4fd
scrambled <scrambled@xirinet.com>
2013-02-16 06:48:00 +0700

    foo

---
+++ bin/reparse-event.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use WR;
+use WR::Process;
+use WR::Events;
+use boolean;
+use MongoDB;
+use Try::Tiny;
+use Data::Dumper;
+
+$| = 1;
+
+use constant WOT_BF_KEY_STR => 'DE 72 BE A0 DE 04 BE B1 DE FE BE EF DE AD BE EF';
+use constant WOT_BF_KEY     => join('', map { chr(hex($_)) } (split(/\s/, WOT_BF_KEY_STR)));
+
+my $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
+my $db     = $mongo->get_database('wot-replays');
+
+my $e      = WR::Events->new(server => 'sea', db => $db);
+my $path = (-e '/home/ben') 
+    ? '/home/ben/projects/wot-replays/data/replays'
+    : '/home/wotreplay/wot-replays/data/replays';
+
+
+foreach my $event (@{$e->events}) {
+    my $res = $e->event($event->{_id});
+    while(my $r = $res->{cursor}->next()) {
+
+    print 'no file', "\n" and next unless(defined($r->{file}));
+    my $process;
+    my $m;
+    my $e;
+    my $f = sprintf('%s/%s', $path, $r->{file});
+
+    try {
+        $process = WR::Process->new(file => $f, db => $db, bf_key => WOT_BF_KEY);
+        $m = $process->process();
+    } catch {
+        $e = $_;
+    };
+
+    $process = undef;
+
+    unless($e) {
+        $m->{site} = $r->{site}; # copy that over
+        $m->{_id}  = $r->{_id}; 
+        $m->{file} = $r->{file};
+        $db->get_collection('replays')->save($m, { safe => 1 });
+        print ': OK', "\n";
+    } else {
+        print ': ERROR: ', $e, "\n";
+    }
+}
+
+}
