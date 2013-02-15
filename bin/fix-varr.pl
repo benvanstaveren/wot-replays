f13cb34edd583cb139e480938ce1c2c198552195
scrambled <scrambled@xirinet.com>
2013-02-16 06:06:05 +0700

    foo

--- bin/fix-varr.pl
+++ bin/fix-varr.pl
@@ -4,9 +4,9 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use MongoDB;
+use Try::Tiny;
 
 $| = 1;
-
 
 my $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
 my $db     = $mongo->get_database('wot-replays');
@@ -15,13 +15,16 @@ my $query = {
     'site.uploaded_at' => {
         '$lte' => time()
     },
+    'vehicles_a' => { '$exists' => 1 },
 };
 
 my $rc = $db->get_collection('replays')->find($query)->sort({ 'site.uploaded_at' => -1 });
-while(my $r = $rc->next()) {
+my $count = $rc->count;
 
+while(my $r = $rc->next()) {
     my $vehicles = $r->{vehicles};
     my $vehicles_a = [];
+    print $r->{_id}->to_string, "\n";
 
     foreach my $id (keys(%$vehicles)) {
         $v = $vehicles->{$id};
@@ -29,6 +32,11 @@ while(my $r = $rc->next()) {
         push(@$vehicles_a, $v);
     }
 
-    $db->get_collection('replays')->update({ _id => $r->{_id} }, { '$set' => { 'vehicles_a' => $vehicles_a } });
-    print $r->{_id}->to_string, "\n";
+    try {
+        $db->get_collection('replays')->update({ _id => $r->{_id} }, { '$set' => { 'vehicles_a' => $vehicles_a } });
+    } catch {
+        $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
+        $rc = $db->get_collection('replays')->find($query)->sort({ 'site.uploaded_at' => -1 });
+        print '-- exception caught, reconnected, ', $rc->count, ' replays left', "\n";
+    };
 }
