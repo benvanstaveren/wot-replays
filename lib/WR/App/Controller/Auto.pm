da03f981b1e63fbad0544e9860053b778db214c6
scrambled <scrambled@xirinet.com>
2013-02-20 13:01:54 +0700

    foo

--- lib/WR/App/Controller/Auto.pm
+++ lib/WR/App/Controller/Auto.pm
@@ -23,23 +23,8 @@ sub index {
         },
     );
 
-    my $req_host;
-    if(my $url = $self->req->url->base) {
-        if($url =~ /http.*?:\/\/(.*?)\/?.*/) {
-            my $host = $1;
-            my @parts = reverse(split(/\./, $host));
-            my $d = $parts[2];
-            $req_host = $d;
-        }
-    }
-
-    $req_host ||= 'www';
-
     $self->app->log->info('url: ' . $self->req->url);
     $self->app->log->info('url->base: ' . $self->req->url->base);
-    $self->app->log->info('req_host: ' . $req_host);
-
-    $self->stash(req_host => $req_host);
 
     # twiddle peoples' openID username and password
     if($self->is_user_authenticated) {
@@ -64,5 +49,3 @@ sub index {
 }
 
 1;
-
-
