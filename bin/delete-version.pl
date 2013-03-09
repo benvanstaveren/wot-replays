a4b652faf71d2c702308c0834c1e6998f530b4d0
scrambled <scrambled@xirinet.com>
2013-03-09 13:48:21 +0700

    foo

--- bin/delete-version.pl
+++ bin/delete-version.pl
@@ -7,23 +7,30 @@ use WR;
 use boolean;
 use MongoDB;
 use Try::Tiny;
+use Data::Dumper;
 
 $| = 1;
 
 die 'Usage: delete-version.pl <version number>', "\n" unless($ARGV[0]);
 
-my $version = $ARGV[0];
+my $version     = $ARGV[0];
 
-my $mongo  = MongoDB::Connection->new();
+# the above will keep versions if they have more than minviews views, or more than minviews downloads
+
+my $mongo  = MongoDB::Connection->new(host => $ENV{MONGO} || 'localhost');
 my $db     = $mongo->get_database('wot-replays');
-my $gfs    = $db->get_gridfs;
+my $coll   = $db->get_collection('replays');
 
-my $cursor = $db->get_collection('replays')->find({ version => $version});
-while(my $o = $cursor->next()) {
-    $gfs->delete($o->{file});
-    $db->get_collection('replays')->update({ _id => 
+print 'Deleting version ', $version, "\n";
+my $cursor = $coll->find({ 'version' => $version });
 
-    
+print 'Have ', $cursor->count, ' replays to delete';
+while(my $r = $cursor->next()) {
+    print $r->{_id}->to_string, ': ';
 
-
-
+    $coll->remove({ _id => $r->{_id} });
+    my $file = sprintf('/home/wotreplay/wot-replays/data/replays/%s', $r->{file});
+    unlink($file);
+    print 'deleted', "\n";
+}
+print 'Done', "\n";
