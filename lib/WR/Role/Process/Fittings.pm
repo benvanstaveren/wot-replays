9d550e1f2922b97fe20673013247333935b80ebf
scrambled <scrambled@xirinet.com>
2013-03-16 15:50:25 +0700

    testes

--- lib/WR/Role/Process/Fittings.pm
+++ lib/WR/Role/Process/Fittings.pm
@@ -1,17 +1,20 @@
 package WR::Role::Process::Fittings;
 use Moose::Role;
 use Try::Tiny;
+use WR::Util::ItemTypes;
 
 around 'process' => sub {
     my $orig = shift;
     my $self = shift;
     my $res  = $self->$orig;
 
+    my $it = WR::Util::ItemTypes;
+
     $res->{vehicle_fittings} = $self->_parser->wot_vehicle_fittings || {};
     $res->{component_attributes} = {
         gun => {
             shot_count => $self->_parser->cb_gun_shot_count->(
-                $self->_parser->player_country,
+                $self->_parser->player_country, # FIXME FIXME relies on block 1 
                 $res->{vehicle_fittings}->{$self->_parser->wot_player_name}->{data}->{gun}
             ),
         },
