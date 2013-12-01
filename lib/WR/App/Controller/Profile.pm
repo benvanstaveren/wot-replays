924f6da7a5bef597ad7b8f97c84eb94a324a3475
scrambled <scrambled@wotreplays.org>
2013-12-01 22:31:30 +0700

    fixes

--- lib/WR/App/Controller/Profile.pm
+++ lib/WR/App/Controller/Profile.pm
@@ -68,10 +68,10 @@ sub replays {
     my $cursor = $self->model('wot-replays.replays')->find($query);
     $cursor->count(sub {
         my ($cursor, $e, $count) = (@_);
-        my $maxp   = int($count/15);
-        $maxp++ if($maxp * 15 < $count);
+        my $maxp   = int($count/10);
+        $maxp++ if($maxp * 10 < $count);
 
-        $cursor->skip( ($page - 1) * 15 );
+        $cursor->skip( ($page - 1) * 10 );
         $cursor->limit(15);
         $cursor->sort({ 'site.uploaded_at' => -1 });
 
