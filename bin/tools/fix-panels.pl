06c956627fe12146ecafd85e835266a16512f8b3
scrambled <scrambled@wotreplays.org>
2013-12-04 12:49:06 +0700

    fix panels fix

--- bin/tools/fix-panels.pl
+++ bin/tools/fix-panels.pl
@@ -14,11 +14,17 @@ my $db     = $mango->db('wot-replays');
 my $coll   = $db->collection('replays');
 my $p = WR::Provider::Panelator->new(db => $db);
 
+
 my $cursor = $coll->find();
+my $total = $cursor->count;
+my $done  = 0;
 while(my $replay = $cursor->next()) {
     if(!defined($replay->{game}->{map_extra})) {
         $replay->{game}->{map_extra} = $p->generate_map_extra($replay);
     }
     $replay->{panel} = $p->panelate($replay);
     $coll->save($replay);
+
+    printf "% 6d / %6d                             \r", ++$done, $total;
 }
+print "\n";
