eecdc6c83a1231f57d79fd23c64d8bea3447e465
scrambled <scrambled@wotreplays.org>
2013-12-15 18:34:22 +0700

    foo

--- bin/tools/reparse-type.pl
+++ bin/tools/reparse-type.pl
@@ -18,16 +18,23 @@ $cursor->sort({ 'site.uploaded_at' => -1 });
 my $done  = 0;
 while(my $replay = $cursor->next()) {
     print 'ID: ', $replay->{_id}, ' DIGEST: ', $replay->{digest}, "\n";
-    $db->collection('jobs')->update({ 
-        _id => $replay->{digest} 
-    }, { 
-        '$set' => {
-            locked      => Mango::BSON::bson_false,
-            ready       => Mango::BSON::bson_true,
-            complete    => Mango::BSON::bson_false,
-            reprocess   => Mango::BSON::bson_true,
-            priority    => 1000,
-            status_text => [],
+    my $job = {
+        _id => $replay->{digest},
+        uploader    => undef,
+        ready       => Mango::BSON::bson_true,
+        reprocess   => Mango::BSON::bson_true,
+        complete    => Mango::BSON::bson_false,
+        status      => 0,
+        error       => undef,
+        replayid    => $replay->{_id},
+        ctime       => Mango::BSON::bson_time,
+        status_text => [],
+        data        => {
+            file    => sprintf('%s/%s', '/home/wotreplay/wot-replays/data/replays', $replay->{file}),
+            desc    => $replay->{site}->{description},
+            visible => ($replay->{site}->{visible}) ? 1 : 0,
+            file_base => $replay->{file},
         },
+        priority    => 1000,
     });
 }
