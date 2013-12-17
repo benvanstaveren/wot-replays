40e50ca7dcf719523a8a5cbd3e78c836f1aac535
scrambled <scrambled@wotreplays.org>
2013-12-17 09:41:34 +0700

    yeah

--- public/js/battleviewer.js
+++ public/js/battleviewer.js
@@ -191,6 +191,10 @@ BasePoint.prototype = {
     captureProgress: function(points, isEnemy) {
         if(this.captured) return;
         if(!this.stateInit) return;
+        if(points == 0) {
+            this.stopCapturing();
+            return;
+        }
         if(isEnemy) {
             this.captureBarP.css({ 'background-color': '#0f0' });
         } else {
