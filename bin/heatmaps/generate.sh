7158dc41d69c916d82e3488b6451bd218e14afaa
scrambled <scrambled@wotreplays.org>
2013-12-19 14:15:35 +0700

    heatmap import

--- bin/heatmaps/generate.sh
+++ bin/heatmaps/generate.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 mongo wtr-heatmaps js/start.js
 for a in `find ../../data/packets/ -name "*.json"`; do
-    script/import.pl $a >> ../../data/packets/import.json;
+    mongoimport --jsonArray -d wtr-heatmaps -c packets $a
 done
-mongoimport -d wtr-heatmaps -c raw_location < ../../data/packets/import.json
+#mongo wtr-heatmaps js/maplist.js
+#mongo wtr-heatmaps js/gameplaylist.js
+#mongo wtr-heatmaps js/bonustypelist.js
+#mongo wtr-heatmaps js/loc-global.js
 
-mongo wtr-heatmaps js/maplist.js
-mongo wtr-heatmaps js/gameplaylist.js
-mongo wtr-heatmaps js/bonustypelist.js
-mongo wtr-heatmaps js/loc-global.js
+#script/export.pl > export.sh
+#rm -rf ../../data/packets/heatmaps/*.json
+#. ./export.sh
 
-script/export.pl > export.sh
-rm -rf ../../data/packets/heatmaps/*.json
-. ./export.sh
-
-rm ../../data/packets/import.json
-rm export.sh
+#rm ../../data/packets/import.json
+#rm export.sh
