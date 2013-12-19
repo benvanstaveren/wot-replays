468b2e83fac4a3874491c2fc98bbe5e2b1366887
scrambled <scrambled@wotreplays.org>
2013-12-19 14:21:58 +0700

    foo

--- bin/heatmaps/script/import.pl
+++ bin/heatmaps/script/import.pl
@@ -16,21 +16,25 @@ my $map_id      = $init->{map_id};
 my $gameplay_id = $init->{gameplay_id};
 my $bonus_type  = $init->{bonus_type};
 
+my $mongo_import;
+
+open($mongo_import, '|/usr/bin/mongoimport -d wtr-heatmaps -c raw_locations') || die ('Could not open pipe to mongoimport', "\n");
+
 # we just dump raw positions and leave it up to the site end to convert this to 
 # the proper subcell values 
 foreach my $p (@$packets) {
     if(defined($p->{position})) {
         $lastpos->{$p->{id}} = $p->{position};
-        print $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type, x => $p->{position}->[0], y => $p->{position}->[2], is_death => 0, is_damage => 0 }), "\n";
+        print $mongo_import $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type, x => $p->{position}->[0], y => $p->{position}->[2], is_death => 0, is_damage => 0 }), "\n";
     }
     if(defined($p->{destroyer})) {
         my $dl = $lastpos->{$p->{destroyed}};
         next unless(defined($dl));
-        print $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type,  x => $dl->[0], y => $dl->[2], is_damage => 0, is_death => 1 }), "\n";
+        print $mongo_import $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type,  x => $dl->[0], y => $dl->[2], is_damage => 0, is_death => 1 }), "\n";
     }
     if(defined($p->{health}) && defined($p->{source})) {
         my $dl = $lastpos->{$p->{id}};
         next unless(defined($dl));
-        print $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type, x => $dl->[0], y => $dl->[2], is_death => 0, is_damage => 1 }), "\n";
+        print $mongo_import $j->encode({ map_id => $map_id, gameplay_id => $gameplay_id, bonus_type => $bonus_type, x => $dl->[0], y => $dl->[2], is_death => 0, is_damage => 1 }), "\n";
     }
 }
