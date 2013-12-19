04dc3de43ece27bcb3e1518e5b9bc1132a0a089e
scrambled <scrambled@wotreplays.org>
2013-12-19 19:53:51 +0700

    yep

--- bin/generate/heatmap_list.pl
+++ bin/generate/heatmap_list.pl
@@ -9,5 +9,6 @@ my $mango  = Mango->new($ENV{'MONGO'} || 'mongodb://localhost:27017');
 my $db     = $mango->db('wot-replays');
 
 foreach my $map (@{$db->collection('data.maps')->find()->sort({ label => 1 })->all()}) {
+    next if($map->{slug} eq 'trainingarea');
     print sprintf(q|<li><a href="/heatmaps/%s">[%% h.loc('%s') %%]</a></li>|, $map->{slug}, $map->{i18n}), "\n";
 }
