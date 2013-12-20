3d7202e25a2c7f767756933c4e6ecdef00db3e61
scrambled <scrambled@wotreplays.org>
2013-12-21 06:15:46 +0700

    yeah

--- lib/WR/API/Auto.pm
+++ lib/WR/API/Auto.pm
@@ -4,10 +4,6 @@ use Mojo::Base 'Mojolicious::Controller';
 # doesn't do anything yet, might be used later to implement blocking and banning
 sub index {
     my $self = shift;
-
-    $self->res->headers->header('Access-Control-Allow-Origin' => '*');
-    $self->res->headers->header('Access-Control-Allow-Headers' => '*');
-
     return 1;
 }
 
