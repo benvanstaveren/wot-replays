f8e144f3e20ea5b2a9b7eac7886cacb69e03c153
scrambled <scrambled@wotreplays.org>
2014-02-07 13:21:10 +0700

    clan stuff

--- bin/tools/clan.pl
+++ bin/tools/clan.pl
@@ -2,6 +2,11 @@
 use strict;
 use warnings;
 use Mojo::UserAgent;
+use Mango;
+
+my $mango  = Mango->new($ENV{'MONGO'} || 'mongodb://localhost:27017');
+my $db     = $mango->db('wot-replays');
+my $coll   = $db->collection('accounts');
 
 my $ua = Mojo::UserAgent->new;
 
@@ -10,7 +15,10 @@ my $url = sprintf('http://statterbox.com/api/v1/%s/clan?server=%s&player=%s', '5
 use Data::Dumper;
 if(my $tx = $ua->get($url)) {
     if(my $res = $tx->success) {
-        print Dumper($res->json);
+        if(my $clan = $res->json->{data}->{$ARGV[1]}) {
+            $coll->update({ _id => sprintf('%s-%s', $ARGV[0], $ARGV[1]) }, { '$set' => { clan => $clan }});
+            print 'UPDATED', "\n";
+        }
     } else {
         print 'REQ ERROR', "\n";
     }
