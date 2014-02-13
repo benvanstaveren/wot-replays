ff481cef4239c82ca6084c020c19706625a2eb5d
scrambled <scrambled@wotreplays.org>
2014-02-13 12:43:52 +0700

    yeah

--- lib/WR/Statterpush/Server.pm
+++ lib/WR/Statterpush/Server.pm
@@ -12,6 +12,8 @@ sub send_to_channel {
     my $message = shift;
     my $cb      = shift;
 
+    warn 'send_to_channel: ', $channel, ' message: ', $message, "\n";
+
     if(!defined($cb)) {
         return $self->ua->post(sprintf('http://api.statterbox.com/push/%s/%s/send_to_channel', $self->token, $self->group) => form => { channel => $channel, message => $message });
     }
