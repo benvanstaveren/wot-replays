c725a75884196bfc64d11033cd87f6c3b55b2c26
scrambled <scrambled@wotreplays.org>
2014-02-14 18:14:36 +0700

    foo

--- lib/WR/App/Controller/Admin/Site.pm
+++ lib/WR/App/Controller/Admin/Site.pm
@@ -48,7 +48,7 @@ sub uploads {
 
         $cursor->skip( ($page - 1) * 50 );
         $cursor->limit(50);
-        $cursor->sort({ 'ctime' => -1, 'priority' => -1 });
+        $cursor->sort({ 'ctime' => -1 }); #, 'priority' => -1 });
         #$cursor->fields({ ctime => 1, error => 1, file => 1, data => 1, uploader => 1, status => 1, replayid => 1 });
 
         $cursor->all(sub {
