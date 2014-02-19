cabd42b3bbb8321366c4d3fc37b05542c2be2d9f
Ben van Staveren <benvanstaveren@gmail.com>
2014-02-19 14:41:30 +0700

    hehm

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,5 +3,5 @@ cd /home/wotreplay/wot-replays/data
 rm -rf mongo
 mkdir mongo
 mongodump -d wot-replays -o /home/wotreplay/wot-replays/data/mongo
-lftp -u $BACKUP_USER,$BACKUP_PASS -e "mirror -RP 4 /home/wotreplay/wot-replays/data/replays wot-replays/replays" ftp://dutchdk.dyndns.org
-lftp -u $BACKUP_USER,$BACKUP_PASS -e "mirror -RP 4 /home/wotreplay/wot-replays/data/mongo wot-replays/mongo" ftp://dutchdk.dyndns.org
+lftp -u $BACKUP_USER,$BACKUP_PASS -c "mirror -RP 4 /home/wotreplay/wot-replays/data/replays wot-replays/replays" ftp://dutchdk.dyndns.org
+lftp -u $BACKUP_USER,$BACKUP_PASS -c "mirror -RP 4 /home/wotreplay/wot-replays/data/mongo wot-replays/mongo" ftp://dutchdk.dyndns.org
