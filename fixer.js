9f6b199fa130014cd23dae49f722866155103dbf
Scrambled <scrambled@wotreplays.org>
2014-06-09 21:02:29 +0700

    heh

--- fixer.js
+++ fixer.js
@@ -1,40 +1,46 @@
 var fixfunction = function(replay) {
-    var server = replay.game.server.toLowerCase();
-    var cid = {};
+    if(replay.game.server != null) {
+        var server = replay.game.server.toLowerCase();
+        var cid = { 
+            player: null,
+            clan: null,
+            involved: {
+                player: [],
+                clan: [],
+                team: [],
+            }
+        };
 
-    cid.player = server + '-' + replay.game.recorder.name.toLowerCase();
-    if(replay.game.recorder.clan != null) {
-        cid.clan = server + '-' + replay.game.recorder.clan.toLowerCase();
-    } else {
-        cid.clan = null;
-    }
+        if(replay.game.recorder.name != null) cid.player = server + '-' + replay.game.recorder.name.toLowerCase();
+        if(replay.game.recorder.clan != null) cid.clan = server + '-' + replay.game.recorder.clan.toLowerCase();
 
-    cid.involved = {
-        player: [],
-        clan: [],
-        team: [],
-    };
+        if(replay.involved != null) {
+            if(replay.involved.players != null) 
+                replay.involved.players.forEach(function(player) {
+                    if(player != null) cid.involved.player.push(server + '-' + player.toLowerCase());
+                });
 
-    cid.involved.players.forEach(function(player) {
-        cid.involved.player.push(server + '-' + player.toLowerCase());
-    });
-    cid.involved.clans.forEach(function(clan) {
-        cid.involved.clan.push(server + '-' + clan.toLowerCase());
-    });
-    cid.involved.team.forEach(function(player) {
-        cid.involved.team.push(server + '-' + player.toLowerCase());
-    });
+            if(replay.involved.clans != null) 
+                replay.involved.clans.forEach(function(clan) {
+                    if(clan != null) cid.involved.clan.push(server + '-' + clan.toLowerCase());
+                });
 
+            if(replay.involved.team != null) 
+                replay.involved.team.forEach(function(player) {
+                    if(player != null) cid.involved.team.push(server + '-' + player.toLowerCase());
+                });
 
-    db.replays.update({ _id: replay._id }, {
-        '$unset': {
-            'game.recorder.cid': 1,
-            'game.recorder.ccid': 1,
-        },
-        '$set': {
-            'cid': cid
+            db.replays.update({ _id: replay._id }, {
+                '$unset': {
+                    'game.recorder.cid': 1,
+                    'game.recorder.ccid': 1
+                },
+                '$set': {
+                    'cid': cid
+                }
+            });
         }
-    });
+    }
 };
 
 //db.replays.find().forEach(fixfunction);
