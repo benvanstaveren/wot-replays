211f4fa3f5b5a8c2bbc721f282f945e7654dbc96
Scrambled <scrambled@wotreplays.org>
2014-09-25 16:18:06 +0700

    hm

--- lib/WR/App/Routes.pm
+++ lib/WR/App/Routes.pm
@@ -19,17 +19,17 @@ sub install {
     });
 
     my $rb = $r->under('/replay/:replay_id');
-        $rb->route('/battleviewer')->to('replays-view#battleviewer', pageid => 'battleviewer', page => { title => 'replay.battleviewer.page.title' });
-        $rb->route('/download')->to('replays-export#download', pageid => undef);
-        $rb->route('/packets')->to('replays-view#packets', pageid => undef);
-        $rb->route('/comment')->to('replays-view#addcomment');
-        $rb->route('/delcomment/:comment_id')->to('replays-view#delcomment');
-        $rb->route('/heatmap')->to('replays-view#heatmap', pageid => 'battleheatmap', page => { title => 'replay.heatmap.page.title' });
-        $rb->route('/desc')->to('replays#desc', pageid => undef);
-        $rb->route('/up')->to('replays-rate#rate_up', pageid => undef);
-        $rb->route('/')->to('replays-view#view', pageid => 'replay')->name('viewreplay');
+        $rb->get('/')->to('replays-view#view', pageid => 'replay')->name('viewreplay');
+        $rb->get('/battleviewer')->to('replays-view#battleviewer', pageid => 'battleviewer', page => { title => 'replay.battleviewer.page.title' });
+        $rb->get('/download')->to('replays-export#download', pageid => undef);
+        $rb->get('/packets')->to('replays-view#packets', pageid => undef);
+        $rb->get('/comment')->to('replays-view#addcomment');
+        $rb->get('/delcomment/:comment_id')->to('replays-view#delcomment');
+        $rb->get('/heatmap')->to('replays-view#heatmap', pageid => 'battleheatmap', page => { title => 'replay.heatmap.page.title' });
+        $rb->get('/desc')->to('replays#desc', pageid => undef);
+        $rb->get('/up')->to('replays-rate#rate_up', pageid => undef);
 
-    $r->route('/')->to('replays#browse', 
+    $r->get('/')->to('replays#browse', 
         filter_opts => {},
         pageid      => 'home', 
         filter_root => 'browse',
@@ -55,8 +55,8 @@ sub install {
         initialize_with => [ '_fp_competitions', '_fp_notifications' ],
     );
 
-    $r->route('/browse/*filter')->to('replays#browse', filter_opts => {}, pageid => 'browse', page => { title => 'browse.page.title' }, filter_root => 'browse');
-    $r->route('/browse')->to(cb => sub {
+    $r->get('/browse/*filter')->to('replays#browse', filter_opts => {}, pageid => 'browse', page => { title => 'browse.page.title' }, filter_root => 'browse');
+    $r->get('/browse')->to(cb => sub {
         my $self = shift;
         $self->stash('browse_filter_raw' => {
             p   => 1,
@@ -75,15 +75,15 @@ sub install {
     });
 
     my $upload = $r->under('/upload');
-        $upload->route('/')->to('replays-upload#upload', pageid => 'upload', upload_type => 'single');
-        $upload->route('/process')->to('replays-upload#process_upload');
-        $upload->route('/:upload_type')->to('replays-upload#upload', pageid => 'upload');
+        $upload->get('/')->to('replays-upload#upload', pageid => 'upload', upload_type => 'single');
+        $upload->any('/process')->to('replays-upload#process_upload');
+        $upload->any('/:upload_type')->to('replays-upload#upload', pageid => 'upload');
 
     my $xhr = $r->under('/xhr');
-        $xhr->route('/qs')->to('ui#xhr_qs');
-        $xhr->route('/ds')->to('ui#xhr_ds');
-        $xhr->route('/du')->to('ui#xhr_du');
-        $xhr->route('/dn_d')->to(cb => sub {
+        $xhr->any('/qs')->to('ui#xhr_qs');
+        $xhr->any('/ds')->to('ui#xhr_ds');
+        $xhr->any('/du')->to('ui#xhr_du');
+        $xhr->any('/dn_d')->to(cb => sub {
             my $self = shift;
             
             $self->render_later;
@@ -97,17 +97,17 @@ sub install {
         });
     
     my $lang = $r->under('/lang');
-        $lang->route('/:lang')->to('ui#xhr_po');
+        $lang->get('/:lang')->to('ui#xhr_po');
 
     my $bv = $r->under('/battleviewer/:replay_id');
-        $bv->route('/')->to('replays-view#battleviewer', pageid => 'battleviewer', page => { title => 'replay.battleviewer.page.title' });
+        $bv->get('/')->to('replays-view#battleviewer', pageid => 'battleviewer', page => { title => 'replay.battleviewer.page.title' });
 
     my $bhm = $r->under('/battleheatmap/:replay_id');
-        $bhm->route('/')->to('replays-view#heatmap', pageid => 'battleheatmap', page => { title => 'replay.heatmap.page.title' });
+        $bhm->get('/')->to('replays-view#heatmap', pageid => 'battleheatmap', page => { title => 'replay.heatmap.page.title' });
 
     $r->route('/clans')->to('clan#index', pageid => 'clan', page => { title => 'clans.page.title' });
     my $clan = $r->under('/clan');
-        $clan->route('/:server/:clanticker')->to(cb => sub {
+        $clan->get('/:server/:clanticker')->to(cb => sub {
             my $self = shift;
             $self->stash('browse_filter_raw' => {
                 p   => 1,
@@ -123,7 +123,7 @@ sub install {
             $self->redirect_to(sprintf('/clan/%s/%s/%s', $self->stash('server'), $self->stash('clanticker'), $self->browse_page(1)));
         }, pageid => 'clan');
 
-        $clan->route('/:server/:clanticker/*filter')->to('replays#browse', next => 'browse',
+        $clan->get('/:server/:clanticker/*filter')->to('replays#browse', next => 'browse',
             page => {
                 title => 'clan.page.title',
                 title_args => [ 'clanticker' ],
@@ -141,9 +141,9 @@ sub install {
             }
         );
 
-    $r->route('/players')->to('player#index', pageid => 'player', page => { title => 'players.page.title' });
+    $r->get('/players')->to('player#index', pageid => 'player', page => { title => 'players.page.title' });
     my $player = $r->under('/player');
-        $player->route('/:server/:player_name')->to(cb => sub {
+        $player->get('/:server/:player_name')->to(cb => sub {
             my $self = shift;
             $self->stash('browse_filter_raw' => {
                 p   => 1,
@@ -158,8 +158,8 @@ sub install {
             });
             $self->redirect_to(sprintf('/player/%s/%s/%s', $self->stash('server'), $self->stash('player_name'), $self->browse_page(1)));
         }, pageid => 'player');
-        $player->route('/:server/:player_name/latest')->to('player#latest', pageid => 'player');
-        $player->route('/:server/:player_name/*filter')->to('replays#browse', next => 'browse',
+        $player->get('/:server/:player_name/latest')->to('player#latest', pageid => 'player');
+        $player->get('/:server/:player_name/*filter')->to('replays#browse', next => 'browse',
             page => {
                 title => 'player.page.title',
                 title_args => [ 'player_name' ],
@@ -179,14 +179,14 @@ sub install {
 
 
     my $vehicles = $r->under('/vehicles');
-        $vehicles->route('/')->to('vehicle#select', 
+        $vehicles->get('/')->to('vehicle#select', 
             pageid  => 'vehicle', 
             page    => { 
                 title   => 'vehicles.page.title', 
                 title_args => [ 't:Select Nation' ],
             }
         );
-        $vehicles->route('/:country')->to('vehicle#index', 
+        $vehicles->get('/:country')->to('vehicle#index', 
             pageid  => 'vehicle', 
             page    => { 
                 title   => 'vehicles.page.title', 
@@ -195,7 +195,7 @@ sub install {
         );
 
     my $vehicle = $r->under('/vehicle');
-        $vehicle->route('/:country/:vehicle')->to(cb => sub {
+        $vehicle->get('/:country/:vehicle')->to(cb => sub {
             my $self = shift;
             $self->stash('browse_filter_raw' => {
                 p   => 1,
@@ -208,7 +208,7 @@ sub install {
             });
             $self->redirect_to(sprintf('/vehicle/%s/%s/%s', $self->stash('country'), $self->stash('vehicle'), $self->browse_page(1)));
         }, pageid => 'vehicle');
-        $vehicle->route('/:country/:vehicle/*filter')->to('replays#browse', 
+        $vehicle->get('/:country/:vehicle/*filter')->to('replays#browse', 
             page => {
                 title       => 'vehicle.page.title',
                 title_args  => [ 'd:data.vehicles:name:vehicle:i18n' ],
@@ -227,15 +227,10 @@ sub install {
         );
 
 
-    $r->route('/maps')->to('map#index', pageid => 'map');
-
-=pod
-    my $heatmaps = $r->under('/heatmaps');
-        $heatmaps->route('/:map_ident')->to('heatmap#view', next => 'view', page => { title => 'heatmaps.page.title', title_args => [ 'map_name' ] });
-=cut
+    $r->get('/maps')->to('map#index', pageid => 'map');
 
     my $map = $r->under('/map');
-        $map->route('/:map_id')->to(cb => sub {
+        $map->get('/:map_id')->to(cb => sub {
             my $self = shift;
             $self->stash('browse_filter_raw' => {
                 p   => 1,
@@ -250,7 +245,7 @@ sub install {
             });
             $self->redirect_to(sprintf('/map/%s/%s', $self->stash('map_id'), $self->browse_page(1)));
         }, pageid => 'map');
-        $map->route('/:map_id/*filter')->to('replays#browse', 
+        $map->get('/:map_id/*filter')->to('replays#browse', 
             page => {
                 title => 'map.page.title',
                 title_args => [ 'map_name' ],
@@ -276,11 +271,11 @@ sub install {
             }
         );
    
-    $r->route('/competitions')->to('competition#list', pageid => 'competition', page => { title => 'competitions.page.title' });
+    $r->get('/competitions')->to('competition#list', pageid => 'competition', page => { title => 'competitions.page.title' });
     my $competition = $r->under('/competition');
         my $cbridge = $competition->bridge('/:competition_id')->to('competition#bridge'); # loads the competition
-            $cbridge->route('/')->to('competition#view', pageid => 'competition', page => { title => 'competition.page.title', title_args => [ 'competition_title' ] });
-            $cbridge->route('/:server/:identifier')->to('replays#browse',
+            $cbridge->get('/')->to('competition#view', pageid => 'competition', page => { title => 'competition.page.title', title_args => [ 'competition_title' ] });
+            $cbridge->get('/:server/:identifier')->to('replays#browse',
                 pageid      => 'competition', 
                 filter_opts => {
                     async      => 1,
@@ -307,7 +302,7 @@ sub install {
                     title_args  => [ 'competition_name', 'identifier' ],
                 }, 
             );
-            $cbridge->route('/:server/:identifier/*filter')->to('replays#browse', 
+            $cbridge->get('/:server/:identifier/*filter')->to('replays#browse', 
                 pageid      => 'competition', 
                 filter_opts => {
                     async      => 1,
@@ -337,10 +332,10 @@ sub install {
 
 
     my $login = $r->under('/login');
-        $login->route('/')->to('auth#do_login', pageid => 'login');
-        $login->route('/:s')->to('auth#do_login', pageid => 'login');
+        $login->any('/')->to('auth#do_login', pageid => 'login');
+        $login->any('/:s')->to('auth#do_login', pageid => 'login');
 
-    $r->route('/logout')->to('auth#do_logout');
+    $r->any('/logout')->to('auth#do_logout');
 
     my $openid = $r->under('/openid');
         $openid->any('/return/:type')->to('auth#openid_return', type => 'default');
@@ -348,72 +343,64 @@ sub install {
 
     my $doc = $r->under('/doc');
         for(qw/about credits missions replayprivacy/) {
-            warn 'doc route returns: ', $doc->route(sprintf('/%s', $_))->to('ui#doc', docfile => $_, pageid => $_), "\n";
+            $doc->get(sprintf('/%s', $_))->to('ui#doc', docfile => $_, pageid => $_);
         }
 
-        $doc->route('/donate')->to(cb => sub {
+        $doc->get('/donate')->to(cb => sub {
             my $self = shift;
             $self->redirect_to('http://www.patreon.com/scrambled');
         });
 
 
     my $pb = $r->bridge('/profile')->to('profile#bridge');
-        $pb->route('/replays/type/:type/page/:page')->to('profile#replays', mustauth => 1, pageid => 'profile', page => { title => 'profile.replays.page.title' });
-        $pb->route('/uploads/page/:page')->to('profile#uploads', pageid => 'profile', page => { title => 'profile.uploads.page.title' });
-        $pb->route('/settings')->to('profile#settings', pageid => 'profile', page => { title => 'profile.settings.page.title' });
-        $pb->route('/sl/:lang')->to('profile#sl', pageid => 'profile');
-        $pb->route('/link')->to('auth#do_link', pageid => 'profile');
-        $pb->route('/link/:s')->to('auth#do_link', pageid => 'profile');
-        $pb->route('/linked/:status')->to('profile#linked', pageid => 'profile');
+        $pb->get('/replays/type/:type/page/:page')->to('profile#replays', mustauth => 1, pageid => 'profile', page => { title => 'profile.replays.page.title' });
+        $pb->get('/uploads/page/:page')->to('profile#uploads', pageid => 'profile', page => { title => 'profile.uploads.page.title' });
+        $pb->any('/settings')->to('profile#settings', pageid => 'profile', page => { title => 'profile.settings.page.title' });
+        $pb->get('/sl/:lang')->to('profile#sl', pageid => 'profile');
+        $pb->get('/link')->to('auth#do_link', pageid => 'profile');
+        $pb->get('/link/:s')->to('auth#do_link', pageid => 'profile');
+        $pb->get('/linked/:status')->to('profile#linked', pageid => 'profile');
 
         my $pbj = $pb->under('/j');
-            $pbj->route('/sr')->to('profile#sr', pageid => 'profile');
-            $pbj->route('/hr')->to('profile#hr', pageid => 'profile');
-            $pbj->route('/pr')->to('profile#pr', pageid => 'profile');
-            $pbj->route('/cr')->to('profile#cr', pageid => 'profile');
-            $pbj->route('/plr')->to('profile#plr', pageid => 'profile');
-            $pbj->route('/tr')->to('profile#tr', pageid => 'profile');
-            $pbj->route('/setting')->to('profile#setting', pageid => 'profile');
+            $pbj->any('/sr')->to('profile#sr', pageid => 'profile');
+            $pbj->any('/hr')->to('profile#hr', pageid => 'profile');
+            $pbj->any('/pr')->to('profile#pr', pageid => 'profile');
+            $pbj->any('/cr')->to('profile#cr', pageid => 'profile');
+            $pbj->any('/plr')->to('profile#plr', pageid => 'profile');
+            $pbj->any('/tr')->to('profile#tr', pageid => 'profile');
+            $pbj->any('/setting')->to('profile#setting', pageid => 'profile');
 
     my $statistics = $r->under('/statistics');
         my $mastery = $statistics->under('/mastery');
-            $mastery->route('/')->to('statistics-mastery#index', pageid => 'statistics/mastery');
-            $mastery->route('/csv/:filedate')->to('statistics-mastery#as_csv', pageid => 'statistics/mastery');
+            $mastery->get('/')->to('statistics-mastery#index', pageid => 'statistics/mastery');
+            $mastery->get('/csv/:filedate')->to('statistics-mastery#as_csv', pageid => 'statistics/mastery');
 
     my $admin = $r->bridge('/admin')->to('admin#bridge');
-        $admin->route('/')->to('admin#index', pageid => 'admin/home');
-        $admin->route('/usersonline')->to('admin#get_online_users');
-        $admin->route('/uploadslist')->to('admin#get_upload_queue');
-        $admin->route('/todaycount')->to('admin#get_today_count');
-        $admin->route('/replaycount')->to('admin#get_replay_count');
+        $admin->get('/')->to('admin#index', pageid => 'admin/home');
+        $admin->get('/usersonline')->to('admin#get_online_users');
+        $admin->get('/uploadslist')->to('admin#get_upload_queue');
+        $admin->get('/todaycount')->to('admin#get_today_count');
+        $admin->get('/replaycount')->to('admin#get_replay_count');
 
-        $admin->route('/events')->to('admin-events#index', pageid => 'admin/events');
+        $admin->get('/events')->to('admin-events#index', pageid => 'admin/events');
 
         my $language = $admin->bridge('/language');
-            $language->route('/')->to('admin-language#redir', pageid => 'admin/language');
+            $language->get('/')->to('admin-language#redir', pageid => 'admin/language');
             my $langroot = $language->bridge('/:lang')->to('admin-language#language_bridge');
-                $langroot->route('/')->to('admin-language#index', pageid => 'admin/language', section => '--');
-                $langroot->route('/publish')->to('admin-language#publish', pageid => 'admin/language');
+                $langroot->get('/')->to('admin-language#index', pageid => 'admin/language', section => '--');
+                $langroot->any('/publish')->to('admin-language#publish', pageid => 'admin/language');
                 my $section = $langroot->under('/:section');
-                    $section->route('/')->to('admin-language#section', pageid => 'admin/language');
-                    $section->route('/single')->via(qw/POST/)->to('admin-language#save_single');
-                    $section->route('/all')->via(qw/POST/)->to('admin-language#save_all');
+                    $section->any('/')->to('admin-language#section', pageid => 'admin/language');
+                    $section->any('/single')->via(qw/POST/)->to('admin-language#save_single');
+                    $section->any('/all')->via(qw/POST/)->to('admin-language#save_all');
 
         my $site = $admin->under('/site');
             my $replays = $site->under('/replays');
-                $replays->route('/page/:page')->to('admin-site#replays', pageid => 'admin/site');
+                $replays->get('/page/:page')->to('admin-site#replays', pageid => 'admin/site');
             my $uploads = $site->under('/uploads');
-                $uploads->route('/page/:page')->to('admin-site#uploads', pageid => 'admin/site');
+                $uploads->get('/page/:page')->to('admin-site#uploads', pageid => 'admin/site');
             my $notifications = $site->under('/notifications');
-                $notifications->route('/')->to('admin-site#notifications', pageid => 'admin/notifications');
-
-        my $modtools = $admin->under('/moderator');
-            my $chatreader = $modtools->under('/chatreader');
-                $chatreader->route('/')->to('admin-moderator-chatreader#index', pageid => 'admin/moderator');
-                $chatreader->route('/process')->to('admin-moderator-chatreader#process');
-            my $bothunter = $modtools->under('/bothunter');
-                $bothunter->route('/')->to('admin-moderator-bothunter#index', pageid => 'admin/moderator');
-                $bothunter->route('/process')->to('admin-moderator-bothunter#process');
+                $notifications->get('/')->to('admin-site#notifications', pageid => 'admin/notifications');
 }
 
 1;
