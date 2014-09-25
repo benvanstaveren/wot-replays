008d04c0b078293f983f58bcb06415a6e3377ff0
Scrambled <scrambled@wotreplays.org>
2014-09-25 14:33:05 +0700

    Preparing for 0.9.3
    - v90300 parser added, adds nothing new
    - Auth plugin reworked to drop the clan check, see if it improves performance any

---
+++ lib/WR/Parser/Versions/v90300.pm
@@ -0,0 +1,4 @@
+package WR::Parser::Versions::v90300;
+use Mojo::Base 'WR::Parser::Versions::v90200';
+
+1;
--- lib/WR/Plugin/Auth.pm
+++ lib/WR/Plugin/Auth.pm
@@ -100,67 +100,22 @@ sub register {
                         } => sub {
                             my ($coll, $err, $oid) = (@_);
                             $self->debug('last seen updated');
-                            $self->model('wot-replays.accounts')->find_one({ _id => $o } => sub {
-                                my ($coll, $err, $user) = (@_);
 
-                                $self->debug('we have that account, yeah: ', Dumper($user));
+                            $self->debug('we have that account, yeah: ', Dumper($user));
+                            $self->debug('expires_at: ', $user->{expires_at}, ' now: ', Mango::BSON::bson_time);
 
-                                $self->debug('expires_at: ', $user->{expires_at}, ' now: ', Mango::BSON::bson_time);
+                            if(defined($user->{expires_at}) && $user->{expires_at} > Mango::BSON::bson_time) {
+                                $self->stash(current_user           => $user);
+                                $self->stash(current_player_name    => $user->{player_name});
+                                $self->stash(current_player_server  => uc($user->{player_server}));
 
-                                if(defined($user->{expires_at}) && $user->{expires_at} > Mango::BSON::bson_time) {
-                                    $self->stash(current_user           => $user);
-                                    $self->stash(current_player_name    => $user->{player_name});
-                                    $self->stash(current_player_server  => uc($user->{player_server}));
-
-                                    $self->current_user->{last_clan_check} ||= 0;
-
-                                    $self->debug('expiry is okay');
-
-                                    if($self->current_user->{last_clan_check} < Mango::BSON::bson_time( (time() - 86400) * 1000)) {
-                                        my $url = 'http://api.statterbox.com/wot/account/clan';
-                                        my $server = lc($self->current_user->{player_server});
-                                        my $name   = $self->current_user->{player_name};
-
-                                        $server = 'asia' if($server eq 'sea');
-
-                                        # lookups by name are expensive, but hey... caching 4tw
-                                        my $form = {
-                                            application_id => $self->stash('config')->{statterbox}->{server},
-                                            name           => lc($name),
-                                            cluster        => $server,
-                                            fields         => 'abbreviation,name,emblems'
-                                        };
-                                        $self->ua->post($url => form => $form, sub {
-                                            my ($ua, $tx) = (@_);
-
-                                            my $clan;
-                                            
-                                            if(my $res = $tx->success) {
-                                                if($res->json('/status') eq 'ok') {
-                                                    $clan = $res->json->{data}->{lc($self->current_user->{player_name})};
-                                                } else {
-                                                    $clan = undef;
-                                                }
-                                            } else {
-                                                $clan = undef;
-                                            }
-                                            $self->current_user->{clan} = $clan;
-                                            $self->model('wot-replays.accounts')->update({ _id => $o }, { '$set' => {
-                                                'last_clan_check' => Mango::BSON::bson_time,
-                                                'clan'            => $clan,
-                                            }} => sub {
-                                                $self->continue;
-                                            });
-                                        });
-                                    } else {
-                                        $self->continue;
-                                    }
-                                } else {
-                                    $self->debug('auth is expired');
-                                    $self->session('openid' => undef, notify => { type => 'info', text => 'Your login has expired, you will have to log in again' });
-                                    $self->continue;
-                                }
-                            });
+                                # last clan check moved to offline worker scripts
+                                $self->continue;
+                            } else {
+                                $self->debug('auth is expired');
+                                $self->session('openid' => undef, notify => { type => 'info', text => 'Your login has expired, you will have to log in again' });
+                                $self->continue;
+                            }
                         });
                     } else {
                         $self->debug('no user data loaded');
