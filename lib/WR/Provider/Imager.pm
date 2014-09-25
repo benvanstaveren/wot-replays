942b814a94d2057a5fc8b0e50ca0bdf1e8cd813c
Scrambled <scrambled@wotreplays.org>
2014-09-26 01:18:33 +0700

    fix

--- lib/WR/Provider/Imager.pm
+++ lib/WR/Provider/Imager.pm
@@ -22,8 +22,8 @@ sub _build_path {
     # this is still cheese, need to either take a root param or figure this out
     # better, we know the 'sites/images...' bit is always the same regardless.
     return (-e '/home/ben') 
-        ? '/home/ben/projects/wot-replays/public',
-        : '/home/wotreplay/wot-replays/public',
+        ? '/home/ben/projects/wot-replays/public'
+        : '/home/wotreplay/wot-replays/public'
         ;
 }
 
