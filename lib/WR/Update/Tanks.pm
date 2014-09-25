265f175fe5829d48178dd649763a1e3925eaaef8
Scrambled <scrambled@wotreplays.org>
2014-09-25 21:34:31 +0700

    yep

--- lib/WR/Update/Tanks.pm
+++ lib/WR/Update/Tanks.pm
@@ -4,11 +4,45 @@ use Mojo::UserAgent;
 
 has 'app' => undef;
 
+sub get_type {
+    my $self = shift;
+    my $tm   = {
+        'lightTank'     =>  'L',
+        'mediumTank'    =>  'M',
+        'heavyTank'     =>  'H',
+        'AT-SPG'        =>  'T',
+        'SPG'           =>  'S',
+    };
+
+    return $tm->{shift};
+}
+
 sub run {
     my $self = shift;
-    use Data::Dumper;
+    my $ua   = Mojo::UserAgent->new;
 
-    die Dumper($self->app->config);
+    if(my $tx = $ua->post('https://api.statterbox.com/wot/encyclopedia/' => form => { cluster => 'asia', language => 'en', application_id => $app->config->{statterbox}->{server} })) {
+        if(my $res = $tx->success) {
+            foreach my $typecomp (keys(%{$res->json->{data}})) {
+                my $vdata = $res->json->{data}->{$typecomp};
+                my $vname  = $vdata->{name};
+                my ($dummy, $ident) = split(/:/, $vname, 2);
+                my $doc   = { 
+                    _id         => sprintf('%s:%s', $vdata->{nation}, $ident),
+                    level       => $vdata->{level} + 0,
+                    name        => $ident,
+                    name_lc     => lc($ident),
+                    country     => $vdata->{nation},
+                    typecomp    => $typecomp + 0,
+                    i18n        => $vdata->{name},
+                    type        => $self->get_type($vdata->{type}),
+                };
+                $app->get_database->collection('data.vehicles')->save($doc);
+            }
+        } else {
+            $app->log->error('Update::Tanks: could not fetch update from encyclopedia');
+        }
+    }
 }
 
 1;
