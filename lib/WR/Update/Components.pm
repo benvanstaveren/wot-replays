f19c1a3a251c5d5f130379eb1b8ec5a7c50eb973
Scrambled <scrambled@wotreplays.org>
2014-09-25 21:58:32 +0700

    ye

--- lib/WR/Update/Components.pm
+++ lib/WR/Update/Components.pm
@@ -1,12 +1,23 @@
 package WR::Update::Components;
 use Mojo::Base '-base';
 use Mojo::UserAgent;
+use Data::Dumper;
 
 has 'app' => undef;
 
 sub run {
     my $self = shift;
+    my $ua   = Mojo::UserAgent->new;
+
+    foreach my $type (qw/engines turrets radios chassis guns/) {
+        if(my $tx = $ua->post(sprintf('https://api.statterbox.com/wot/encyclopedia/tank%s/', $type) => form => { cluster => 'asia', language => 'en', application_id => $self->app->config->{statterbox}->{server} })) {
+            if(my $res = $tx->success) {
+                die Dumper($res->json);
+            } else {
+                $self->app->log->error('Update::Components: could not fetch update for ' . $type . ' from encyclopedia: ' . Dumper($tx->error));
+            }
+        }
+    }
 }
 
 1;
-
