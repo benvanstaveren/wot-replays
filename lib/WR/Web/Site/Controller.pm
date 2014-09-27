3788e6feda771adb73a0d5e7993f8a47060eec51
Scrambled <scrambled@wotreplays.org>
2014-09-28 01:43:24 +0700

    more shuffling

--- lib/WR/Web/Site/Controller.pm
+++ lib/WR/Web/Site/Controller.pm
@@ -1,7 +1,5 @@
-package WR::App::Controller;
+package WR::Web::Site::Controller;
 use Mojo::Base 'Mojolicious::Controller';
-use Mango::BSON;
-use Data::Dumper;
 
 sub respond {
     my $self = shift;
@@ -9,10 +7,6 @@ sub respond {
     my $stash = delete($args{'stash'});
 
     $self->stash(%$stash) if(defined($stash));
-    if(my $start = $self->stash('timing.start')) {
-        $self->stash('timing_elapsed' => Time::HiRes::tv_interval($start));
-    }
-    $self->debug('respond: about to respond using self->render, args: ', Dumper({%args}));
     $self->render(%args);
 }
 
