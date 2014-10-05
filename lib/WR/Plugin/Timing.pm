fd32d0807794fd4f968275eb56ce36cac6cb7f59
Scrambled <scrambled@wotreplays.org>
2014-10-05 18:32:00 +0700

    alter timings

--- lib/WR/Plugin/Timing.pm
+++ lib/WR/Plugin/Timing.pm
@@ -9,13 +9,40 @@ sub register {
 
     $app->hook(before_dispatch => sub {
         my $c = shift;
-        $c->stash('timing.start' => [ gettimeofday ]);
+        $c->stash('timing' => { start => [ gettimeofday ], hooks => {} });
+    });
+    
+    $app->hook(after_static => sub {
+        my $c = shift;
+        $c->stash('timing')->{hooks}->{after_static} = tv_interval($c->stash('timing')->{start});
+    });
+
+    $app->hook(before_routes => sub {
+        my $c = shift;
+        $c->stash('timing')->{hooks}->{before_routes} = tv_interval($c->stash('timing')->{start});
+    });
+
+    $app->hook(around_action => sub {
+        my ($next, $c, $action, $last) = (@_);
+        $c->stash('timing')->{hooks}->{around_action} = tv_interval($c->stash('timing')->{start});
+        return $next->();
+    });
+
+    $app->helper('full_timing_list' => sub {
+        my $self = shift;
+        my $l    = [];
+
+        for(qw/after_static before_routes around_action/) {
+            push(@$l, sprintf('%s: %.4f', $_, $self->stash('timing')->{hooks}->{$_}));
+        }
+        push(@$l, sprintf('total: %.4f', $self->stash('timing_elapsed')));
+
+        return join(', ', @$L);
     });
 
     $app->hook(before_render => sub {
         my ($c, $args) = (@_);
-        $c->stash('timing.elapsed' => tv_interval($c->stash('timing.start')));
-        $c->stash('timing_elapsed' => $c->stash('timing.elapsed'));
+        $c->stash('timing_elapsed' => tv_interval($c->stash('timing')->{start}));
     });
 }
 
