441a1717c7bec4cbe31a0c25cbb964ba0437a090
Scrambled <scrambled@wotreplays.org>
2014-10-06 03:55:36 +0700

    postaction fix

--- lib/WR/Web/Site/Controller/Postaction.pm
+++ lib/WR/Web/Site/Controller/Postaction.pm
@@ -8,12 +8,10 @@ sub preview {
     my $ip   = $self->req->param('i');
     my $r    = $self->req->param('r');
 
-    $self->debug('preview postaction, file: ', $file, ' stat: ', $stat, ' ip: ', $ip, ' ref: ', $r);
-
     $self->render_later;
     if(defined($stat) && lc($stat) eq 'ok') {
         my $real_file = substr($file, 1); # because we want to ditch that leading slash
-        $self->_piwik_track_download($file => $ip => 'previews.wotreplays.org' => sub {
+        $self->_piwik_track_download({ file => $file, ip => $ip, host => 'previews.wotreplays.org', ref => $r } => sub {
             $self->render(text => 'OK');
         });
     } else {
@@ -26,6 +24,7 @@ sub replay {
     my $file = $self->req->param('f');
     my $stat = $self->req->param('s');
     my $ip   = $self->req->param('i');
+    my $r    = $self->req->param('r');
 
     $self->render_later;
     if(defined($stat) && lc($stat) eq 'ok') {
@@ -40,7 +39,7 @@ sub replay {
                 my ($c, $e, $d) = (@_);
 
                 if(defined($d)) {
-                    $self->_piwik_track_download($file => $ip => 'dl.wotreplays.org' => sub {
+                    $self->_piwik_track_download({ file => $file, ip => $ip, host => 'dl.wotreplays.org', ref => $r } => sub {
                         $self->app->thunderpush->send_to_channel('site' => Mojo::JSON->new->encode({ evt => 'replay.download', data => { id => $d->{_id} . '' } }) => sub {
                             my ($p, $r) = (@_);
                             $self->render(text => 'OK');
@@ -58,16 +57,20 @@ sub replay {
 
 sub _piwik_track_download {
     my $self = shift;
-    my $file = shift;
-    my $ip   = shift;
-    my $host = shift;
+    my $args = shift;
     my $cb   = shift;
+
+    my $file = $args->{file};
+    my $ip   = $args->{ip};
+    my $host = $args->{host};
+    my $ref  = $args->{ref};
 
     $self->ua->get($self->get_config('piwik.url') => form => {
         idsite          => 1,
         token_auth      => $self->get_config('piwik.token_auth'),
         rec             => 1,
         url             => sprintf('http://%s%s', $host, $file),
+        urlref          => $ref,
         action_name     => ($host eq 'dl.wotreplays.org') ? 'Replay/Download' : 'Replay/Preview',
         apiv            => 1,
         download        => sprintf('http://%s%s', $host, $file),
