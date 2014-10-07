86719e764ccb6523d52727cb2ad0e8de9a424055
Scrambled <scrambled@wotreplays.org>
2014-10-07 08:37:35 +0700

    just to keep things sane

--- bin/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/wotreplay/wot-replays/data
-rm -rf mongo
-mkdir mongo
-mongodump -d wot-replays -o /home/wotreplay/wot-replays/data/mongo
-lftp -u $BACKUP_USER,$BACKUP_PASS -e "mirror -RP 4 /home/wotreplay/wot-replays/data/replays wot-replays/replays ; exit" ftp://dutchdk.dyndns.org
-lftp -u $BACKUP_USER,$BACKUP_PASS -e "mirror -RP 4 /home/wotreplay/wot-replays/data/mongo wot-replays/mongo ; exit" ftp://dutchdk.dyndns.org
--- bin/heatmaps/js/start.js
+++
@@ -1,1 +0,0 @@
-db['raw_location'].ensureIndex({ map_id: 1, gameplay_id: 1, bonus_type: 1, is: 1 });
